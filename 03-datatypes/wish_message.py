2731d6ccae21bd7e5f5d763d206fc2542ef68dce
Rikhi <rikhiram47@gmail.com>
2025-10-01 05:24:54 +0530

    mini project for day 1

---
+++ 03-datatypes/wish_message.py
@@ -0,0 +1,21 @@
+
+from datetime import datetime
+
+
+name = input("Enter your name: ")
+
+wish = ''
+
+hour =  datetime.now().hour
+
+if hour < 12:
+    wish = 'Good Morning, ' + name
+elif hour >= 12 and hour < 16:
+    wish = 'Good Afternoon, ' + name
+elif hour >= 16 and hour < 20:
+    wish = 'Good Evening, ' + name
+else:
+    wish = 'Good Night, ' + name
+
+print(wish)
+
